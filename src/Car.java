1458f08d65e42bec2b008ee00515f1731a987e7d
roman <roman-1901@mail.ru>
2022-12-12 23:05:30 +0300

    cars

--- src/Car.java
+++ src/Car.java
@@ -7,6 +7,24 @@ public class Car {
     final String country;
 
     public Car(String brand, String model, double engineVolume, String color, int year, String country) {
+        if (brand == null || brand.isEmpty()) {
+            brand = "default";
+        }
+        if (model == null || model.isEmpty()) {
+            model = "default";
+        }
+        if (country == null || country.isEmpty()) {
+            country = "default";
+        }
+        if (engineVolume <= 0) {
+            engineVolume = 1.5;
+        }
+        if (color == null || color.isEmpty()) {
+            color = "белый";
+        }
+        if (year <= 0) {
+            year = 2000;
+        }
         this.brand = brand;
         this.model = model;
         this.engineVolume = engineVolume;
